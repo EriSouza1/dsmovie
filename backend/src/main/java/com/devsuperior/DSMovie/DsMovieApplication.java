646bd207a280d24ba0e0684f1eb62d32b9d33f26
Eri Souza <eri.ferreira34@gmail.com>
2022-05-09 10:42:38 -0300

    Project Created

---
+++ backend/src/main/java/com/devsuperior/DSMovie/DsMovieApplication.java
@@ -0,0 +1,13 @@
+package com.devsuperior.DSMovie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DsMovieApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DsMovieApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/devsuperior/DSMovie/DsMovieApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devsuperior.DSMovie;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DsMovieApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
