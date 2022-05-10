d740b7e42de24d2dde39d188c589af0055b699da
Eri Souza <eri.ferreira34@gmail.com>
2022-05-10 16:40:06 -0300

    MovieCard

--- frontend/src/pages/listing/index.tsx
+++ frontend/src/pages/listing/index.tsx
@@ -1,11 +1,37 @@
 import Pagination from "Componentes/Pagination";
+import MovieCard from "Componentes/MovierCard";
 
 function listing() {
-    
+
     return (
-        <Pagination />
+        <>
+            <Pagination />
+
+            <div className="Container">
+                <div className="row">
+                    <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
+                        <MovieCard />
+                    </div>
+                    <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
+                        <MovieCard />
+                    </div>
+                    <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
+                        <MovieCard />
+                    </div>
+                    <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
+                        <MovieCard />
+                    </div>
+                    <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
+                        <MovieCard />
+                    </div>
+                </div>
+            </div>
+       
+
+
+        </>
     );
-    
+
 }
 
 export default listing;
