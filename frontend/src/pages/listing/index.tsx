72446492a26f832d956cd9834b95ed8d8535492c
Eri Souza <eri.ferreira34@gmail.com>
2022-05-14 16:37:00 -0300

    First Request

--- frontend/src/pages/listing/index.tsx
+++ frontend/src/pages/listing/index.tsx
@@ -1,7 +1,16 @@
 import Pagination from "Componentes/Pagination";
 import MovieCard from "Componentes/MovierCard";
+import axios from "axios";
+import { textChangeRangeIsUnchanged } from "typescript";
+import { BASE_URL } from "utils/requests";
 
 function listing() {
+
+    // FORMA ERRADA
+    axios.get(`${BASE_URL}/movies?size=12&page=0`)
+        .then(response => {
+            console.log(response.data);
+        })
 
     return (
         <>
