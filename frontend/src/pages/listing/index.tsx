3a3a0f1d08de9f958021c5a1054453f83be1e22f
Eri Souza <eri.ferreira34@gmail.com>
2022-05-14 17:36:36 -0300

    useState, useEffect

--- frontend/src/pages/listing/index.tsx
+++ frontend/src/pages/listing/index.tsx
@@ -1,19 +1,30 @@
 import Pagination from "Componentes/Pagination";
 import MovieCard from "Componentes/MovierCard";
 import axios from "axios";
-import { textChangeRangeIsUnchanged } from "typescript";
 import { BASE_URL } from "utils/requests";
+import { useEffect, useState } from "react";
+import { MoviePage } from "types/movie";
 
-function listing() {
+function Listing() {
 
-    // FORMA ERRADA
-    axios.get(`${BASE_URL}/movies?size=12&page=0`)
+   
+    const [pageNumber, setPageNumber] = useState(0);
+
+   
+    useEffect(() => {
+        axios.get(`${BASE_URL}/movies?size=12&page=1`)
         .then(response => {
-            console.log(response.data);
-        })
+            const data = response.data as MoviePage;
+            console.log(data);
+            setPageNumber(data.number);
+        });
+    }, []);
 
+
+  
     return (
         <>
+        <p>{pageNumber}</p>
             <Pagination />
 
             <div className="Container">
@@ -43,4 +54,4 @@ function listing() {
 
 }
 
-export default listing;
+export default Listing;
